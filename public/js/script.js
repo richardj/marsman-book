c8dd1c72dfb9d3883aabd1c13d7d0430d08213fc
Richard Japenga <treurmars@gmail.com>
2015-05-04 15:58:42 +0200

    change trigger text when expanding / collapsing

--- public/js/script.js
+++ public/js/script.js
@@ -6,15 +6,16 @@ navigation = function() {
   var nav = document.getElementById('navigation');
   var trigger = nav.querySelectorAll('span')[0];
 
-  trigger.addEventListener("click", expandMenu);
+  trigger.addEventListener("click", toggleMenu);
 
-  function expandMenu(event) {
+  function toggleMenu(event) {
     if (nav.classList.contains('active')) {
       nav.classList.remove('active');
+      trigger.innerHTML = "Menu +";
     }
     else {
       nav.classList.add('active');
+      trigger.innerHTML = "Menu -";
     }
-    
   }
 }
